7825d52f702dbcf7ce015e1a370fdfbeb03ab703
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-07-14 09:07:00 -0700

    updated examples

--- examples/src/main/scala/ScalaOpenKoreanTextExample.scala
+++ examples/src/main/scala/ScalaOpenKoreanTextExample.scala
@@ -32,13 +32,7 @@ object ScalaOpenKoreanTextExample {
     // Tokenize
     val tokens: Seq[KoreanToken] = OpenKoreanTextProcessor.tokenize(normalized)
     println(tokens)
-    // List(한국어(Noun: 0, 3), 를(Josa: 3, 1),  (Space: 4, 1), 처리(Noun: 5, 2), 하는(Verb: 7, 2),  (Space: 9, 1), 예시(Noun: 10, 2), 입니(Adjective: 12, 2), 다(Eomi: 14, 1), ㅋㅋ(KoreanParticle: 15, 2),  (Space: 17, 1), #한국어(Hashtag: 18, 4))
-
-    // Stemming
-    val stemmed: Seq[KoreanToken] = OpenKoreanTextProcessor.stem(tokens)
-
-    println(stemmed)
-    // List(한국어(Noun: 0, 3), 를(Josa: 3, 1),  (Space: 4, 1), 처리(Noun: 5, 2), 하다(Verb: 7, 2),  (Space: 9, 1), 예시(Noun: 10, 2), 이다(Adjective: 12, 3), ㅋㅋ(KoreanParticle: 15, 2),  (Space: 17, 1), #한국어(Hashtag: 18, 4))
+    // List(한국어(Noun: 0, 3), 를(Josa: 3, 1),  (Space: 4, 1), 처리(Noun: 5, 2), 하는(Verb(하다): 7, 2),  (Space: 9, 1), 예시(Noun: 10, 2), 입니다(Adjective(이다): 12, 3), ㅋㅋㅋ(KoreanParticle: 15, 3),  (Space: 18, 1), #한국어(Hashtag: 19, 4))
 
     // Phrase extraction
     val phrases: Seq[KoreanPhrase] = OpenKoreanTextProcessor.extractPhrases(tokens, filterSpam = true, enableHashtags = true)
